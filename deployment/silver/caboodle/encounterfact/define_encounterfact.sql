7fb8d74e0d0021ca62fc507d7a6e2f22c9bb3c38
Nikolai Nielsen <nikolai.plambech.nielsen.01@regionh.dk>
2025-03-25 10:05:54 +0100

    Update define_encounterfact.sql

--- deployment/silver/caboodle/encounterfact/define_encounterfact.sql
+++ deployment/silver/caboodle/encounterfact/define_encounterfact.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS dap_x_silver.caboodle.patientdim (
     encounterkey bigint not null,
     patientdurablekey bigint,
     departmentkey bigint,
-    visitkey bigint,
-    morecolumns string
+    visitkey bigint
 )
